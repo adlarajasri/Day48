e0f9a7c587f06ad5e43e8365c381dbb87aeaa1d7
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-06-03 14:50:09 +0530

    Add files via upload

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+public class MainClass
+
+{
+
+    public static void PrintArray<T>(T[] array)
+
+    {
+
+        foreach (T element in array)
+
+        {
+
+            Console.WriteLine(element);
+
+        }
+
+    }
+
+    public static void Main(string[] args)
+
+    {
+
+        int[] intArray = { 1, 2, 3, 4, 5 };
+
+        string[] stringArray = { "Hello", "World", "C#", "Generics" };
+
+        PrintArray(intArray);
+
+        PrintArray(stringArray);
+
+    }
+
+}
