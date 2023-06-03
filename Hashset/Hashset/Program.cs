2ee893855cdcfb6b86b8a33b01a9d167088ebbd3
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-06-03 14:50:32 +0530

    Add files via upload

---
+++ Hashset/Hashset/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+public class MainClass
+
+{
+
+    public static void Main(string[] args)
+
+    {
+
+        HashSet<string> uniquePairs = new HashSet<string>();
+
+        Console.Write("Enter the number of pairs: ");
+
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the pairs separated by a space:");
+
+        for (int i = 0; i < n; i++)
+
+        {
+
+            string pair = Console.ReadLine();
+
+            uniquePairs.Add(pair);
+
+            Console.WriteLine(uniquePairs.Count);
+
+        }
+
+    }
+
+}
